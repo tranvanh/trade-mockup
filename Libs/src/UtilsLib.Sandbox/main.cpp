c028ae3dbe36508b0da87cf60f708c05ec1cd04b
tranvanh <tranvietanhcz@gmail.com>
2025-12-05 11:38:20 +0100

    Add UtilsLib sandbox

---
+++ Libs/src/UtilsLib.Sandbox/main.cpp
@@ -0,0 +1,16 @@
+#include "UtilsLib/Common.h"
+#include <iostream>
+
+TRANVANH_NAMESPACE_BEGIN
+
+void sandbox() {
+    // Add sandbox code
+    std::cout << "Hello World!\n";
+}
+
+TRANVANH_NAMESPACE_END
+
+int main() {
+    tranvanh::sandbox();
+    return 0;
+}
