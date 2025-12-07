64494b40a9277192cae3cf80eade2134ab3290cc
tranvanh <tranvietanhcz@gmail.com>
2025-12-07 01:44:56 +0100

    add flatmap implementation

--- Libs/src/UtilsLib.Benchmark/FlatMap.Benchmark.cpp
+++ Libs/src/UtilsLib.Benchmark/FlatMap.Benchmark.cpp
@@ -3,10 +3,10 @@
 #include <benchmark/benchmark.h>
 
 #include "UtilsLib/Common.h"
+#include "UtilsLib/FlatMap.h"
 
 TRANVANH_NAMESPACE_BEGIN
 
-// Benchmark registering a single order to a filled book
 static void BM_StdVector_LinearRead(benchmark::State& state) {
     std::vector<int> testVector(state.range(0), 0);
 
@@ -21,7 +21,21 @@ static void BM_StdVector_LinearRead(benchmark::State& state) {
 
 BENCHMARK(BM_StdVector_LinearRead)->Arg(10000);
 
-// Benchmark registering a single order to a filled book
+static void BM_StdMap_LinearInsert(benchmark::State& state) {
+    std::map<int, int> testMap;
+    const int endRange = state.range(0);
+    for (auto _ : state) {
+        for (int i = 0; i < endRange; ++i) {
+            testMap.insert({i, 0});
+        }
+        benchmark::ClobberMemory();
+        state.PauseTiming();
+        testMap.clear();
+        state.ResumeTiming();
+    }
+}
+BENCHMARK(BM_StdMap_LinearInsert)->Arg(10000);
+
 static void BM_StdMap_LinearReverseInsert(benchmark::State& state) {
     std::map<int, int> testMap;
     int endRange = state.range(0);
@@ -35,9 +49,8 @@ static void BM_StdMap_LinearReverseInsert(benchmark::State& state) {
         state.ResumeTiming();
     }
 }
-BENCHMARK(BM_StdMap_LinearReverseInsert)->Arg(10000);
+BENCHMARK(BM_StdMap_LinearReverseInsert)->Arg(1000);
 
-// Benchmark registering a single order to a filled book
 static void BM_StdMap_LinearRead(benchmark::State& state) {
     std::map<int, int> testMap;
     int endRange = state.range(0);
@@ -54,5 +67,53 @@ static void BM_StdMap_LinearRead(benchmark::State& state) {
     }
 }
 BENCHMARK(BM_StdMap_LinearRead)->Arg(10000);
+
+static void BM_FlatMap_LinearInsert(benchmark::State& state) {
+    FlatMap<int, int> testMap;
+    const int endRange = state.range(0);
+    for (auto _ : state) {
+        for (int i = 0; i < endRange; ++i) {
+            testMap.insert({i, 0});
+        }
+        benchmark::ClobberMemory();
+        state.PauseTiming();
+        testMap.clear();
+        state.ResumeTiming();
+    }
+}
+BENCHMARK(BM_FlatMap_LinearInsert)->Arg(10000);
+
+static void BM_FlatMap_LinearReverseInsert(benchmark::State& state) {
+    FlatMap<int, int> testMap;
+    int endRange = state.range(0);
+    for (auto _ : state) {
+        for (int i = endRange; i >= 0; --i) {
+            testMap.insert({i, 0});
+        }
+        benchmark::ClobberMemory();
+        state.PauseTiming();
+        testMap.clear();
+        state.ResumeTiming();
+    }
+}
+BENCHMARK(BM_FlatMap_LinearReverseInsert)->Arg(1000);
+
+static void BM_FlatMap_LinearRead(benchmark::State& state) {
+    FlatMap<int, int> testMap;
+    int endRange = state.range(0);
+    for (int i = endRange; i >= 0; --i) {
+        testMap.insert({i, 0});
+    }
+
+    for (auto _ : state) {
+        for (const auto& item : testMap) {
+            auto read = item;
+            benchmark::DoNotOptimize(read.first);
+            benchmark::DoNotOptimize(read.second);
+        }
+        benchmark::ClobberMemory();
+    }
+}
+BENCHMARK(BM_FlatMap_LinearRead)->Arg(10000);
 
 TRANVANH_NAMESPACE_END
---
+++ Libs/src/UtilsLib/FlatMap.h
@@ -0,0 +1,97 @@
+#include "UtilsLib/Common.h"
+#include "UtilsLib/Serialization.h"
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+TRANVANH_NAMESPACE_BEGIN
+
+template <std::totally_ordered TKey, typename TValue, size_t TInitSize = 10>
+class FlatMap {
+    using Item          = std::pair<TKey, TValue>;
+    using Iterator      = std::vector<Item>::iterator;
+    using ConstIterator = std::vector<Item>::const_iterator;
+
+    std::vector<Item>                             mData;
+    std::function<bool(const Item&, const Item&)> mComp;
+
+    size_t mFilledSize = 0;
+
+public:
+    FlatMap()
+        : mComp([](const Item& a, const Item& b) {
+            return a.first < b.first;
+        }) {
+        mData.reserve(TInitSize);
+    }
+    FlatMap(std::function<bool(const TKey&, const TKey&)>&& comp)
+        : mComp([comp = std::move(comp)](const Item& a, const Item& b) {
+            return comp(a.first, b.first);
+        }) {
+        mData.reserve(TInitSize);
+    }
+
+    bool erase(const TKey& key) {
+        auto pos = find(key);
+        if (pos == end()) {
+            return false;
+        }
+        mData.erase(pos);
+        --mFilledSize;
+        return true;
+    }
+
+    bool insert(const Item& item) {
+        auto range = std::equal_range(mData.begin(), mData.begin()+mFilledSize, item, mComp);
+        if (range.first != range.second) {
+            return false;
+        }
+        mData.insert(range.first, item);
+        ++mFilledSize;
+        return true;
+    }
+
+    bool insert(Item&& item) {
+        auto range = std::equal_range(mData.begin(), mData.begin()+mFilledSize, item, mComp);
+        if (range.first != range.second) {
+            return false;
+        }
+        mData.insert(range.first, std::move(item));
+        ++mFilledSize;
+        return true;
+    }
+
+    Iterator find(const TKey& key) {
+        auto range = std::equal_range(mData.begin(), mData.end(), std::make_pair(key, 0), mComp);
+        return range.first == range.second ? end() : range.first;
+    }
+
+    ConstIterator find(const TKey& key) const {
+        auto range = std::equal_range(mData.cbegin(), mData.cend(), std::make_pair(key, 0), mComp);
+        return range.first == range.second ? cend() : range.first;
+    }
+
+    std::optional<TValue> get(const TKey& key) const {
+        auto pos = find(key);
+        return pos != end() ? pos->second : std::nullopt;
+    }
+
+    bool contains(const TKey& key) const { return find(key) != end(); }
+
+    bool empty() const { return mData.empty(); }
+    size_t size() const { return mData.size(); }
+    void clear() {
+        mData.clear();
+        mFilledSize = 0;
+        mData.reserve(TInitSize);
+    }
+    Iterator      begin() { return mData.begin(); }
+    ConstIterator begin() const { return mData.begin(); }
+    ConstIterator cbegin() const noexcept { return mData.cbegin(); }
+
+    Iterator      end() { return mData.end(); }
+    ConstIterator end() const { return mData.end(); }
+    ConstIterator cend() const noexcept { return mData.cend(); }
+};
+
+TRANVANH_NAMESPACE_END
