6df7e04cd4b4e6b79116f0f0b262647edd117332
tranvanh <tranvietanhcz@gmail.com>
2026-04-26 14:55:19 +0200

    remove global lock

--- TradeGUI/src/UIState.cpp
+++ TradeGUI/src/UIState.cpp
@@ -1,65 +1,64 @@
 #include "TradeGUI/UIState.h"
-#include <algorithm>
 
 namespace tradegui {
 
 void UIState::pushTrade(std::string line) {
-    std::lock_guard lock(mtx);
-    if (trades.size() >= MAX_TRADE_LINES)
-        trades.pop_front();
-    trades.push_back(std::move(line));
-    ++totalTrades;
+    std::lock_guard lock(trades.mtx);
+    if (trades.data.size() >= MAX_TRADE_LINES)
+        trades.data.pop_front();
+    trades.data.push_back(std::move(line));
+    ++trades.totalTrades;
 }
 
 void UIState::pushLog(std::string line) {
-    std::lock_guard lock(mtx);
-    if (logs.size() >= MAX_LOG_LINES)
-        logs.pop_front();
-    logs.push_back(std::move(line));
+    std::lock_guard lock(logs.mtx);
+    if (logs.data.size() >= MAX_LOG_LINES)
+        logs.data.pop_front();
+    logs.data.push_back(std::move(line));
 }
 
 void UIState::pushCmd(std::string line) {
-    std::lock_guard lock(mtx);
-    if (cmdHistory.size() >= MAX_CMD_HISTORY)
-        cmdHistory.pop_front();
-    cmdHistory.push_back(std::move(line));
+    std::lock_guard lock(cmdHistory.mtx);
+    if (cmdHistory.data.size() >= MAX_CMD_HISTORY)
+        cmdHistory.data.pop_front();
+    cmdHistory.data.push_back(std::move(line));
 }
 
 void UIState::addConnection(unsigned short port) {
-    std::lock_guard lock(mtx);
-    connections.push_back(port);
+    std::lock_guard lock(connections.mtx);
+    connections.data.push_back(port);
 }
 
 void UIState::removeConnection(unsigned short port) {
-    std::lock_guard lock(mtx);
-    auto it = std::find(connections.begin(), connections.end(), port);
-    if (it != connections.end())
-        connections.erase(it);
+    std::lock_guard lock(connections.mtx);
+    auto it = std::find(connections.data.begin(), connections.data.end(), port);
+    if (it != connections.data.end())
+        connections.data.erase(it);
 }
 
 std::deque<std::string> UIState::snapshotTrades() const {
-    std::lock_guard lock(mtx);
-    return trades;
+    std::lock_guard lock(trades.mtx);
+    return trades.data;
 }
 
 std::deque<std::string> UIState::snapshotLogs() const {
-    std::lock_guard lock(mtx);
-    return logs;
+    std::lock_guard lock(logs.mtx);
+    return logs.data;
 }
 
 std::deque<std::string> UIState::snapshotCmds() const {
-    std::lock_guard lock(mtx);
-    return cmdHistory;
+    std::lock_guard lock(cmdHistory.mtx);
+    return cmdHistory.data;
 }
 
 std::vector<unsigned short> UIState::snapshotConnections() const {
-    std::lock_guard lock(mtx);
-    return connections;
+    std::lock_guard lock(connections.mtx);
+    return connections.data;
 }
 
 std::size_t UIState::snapshotTotalTrades() const {
-    std::lock_guard lock(mtx);
-    return totalTrades;
+    std::lock_guard lock(trades.mtx);
+    return trades.totalTrades;
 }
 
 } // namespace tradegui
